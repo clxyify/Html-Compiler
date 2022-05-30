6ae9170b5e07fa8ef49e923c3ecda402138cb45c
clxyify <98536033+clxyify@users.noreply.github.com>
2022-05-30 18:34:33 -0400

    Add files via upload

---
+++ HTML Compiler/HTML Compiler/Compiler.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace HTML_Compiler
+{
+    partial class Compiler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Compiler));
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveHTMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshHTMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.developedByClxyifyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoIndentCharsPatterns = "";
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(452, 16);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.CharHeight = 16;
+            this.fastColoredTextBox1.CharWidth = 9;
+            this.fastColoredTextBox1.CommentPrefix = null;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.FoldingIndicatorColor = System.Drawing.Color.Silver;
+            this.fastColoredTextBox1.Font = new System.Drawing.Font("Courier New", 11F);
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.HTML;
+            this.fastColoredTextBox1.LeftBracket = '<';
+            this.fastColoredTextBox1.LeftBracket2 = '(';
+            this.fastColoredTextBox1.LineNumberColor = System.Drawing.Color.Black;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(7, 389);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.RightBracket = '>';
+            this.fastColoredTextBox1.RightBracket2 = ')';
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(703, 100);
+            this.fastColoredTextBox1.TabIndex = 0;
+            this.fastColoredTextBox1.Text = "<p>This is an example of the HTML compiler!</p>";
+            this.fastColoredTextBox1.Zoom = 100;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(9, 28);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(701, 355);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.saveHTMLToolStripMenuItem,
+            this.refreshHTMLToolStripMenuItem,
+            this.developedByClxyifyToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(716, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.fileToolStripMenuItem.Text = "Compile HTML";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // saveHTMLToolStripMenuItem
+            // 
+            this.saveHTMLToolStripMenuItem.Name = "saveHTMLToolStripMenuItem";
+            this.saveHTMLToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.saveHTMLToolStripMenuItem.Text = "Save HTML";
+            this.saveHTMLToolStripMenuItem.Click += new System.EventHandler(this.saveHTMLToolStripMenuItem_Click);
+            // 
+            // refreshHTMLToolStripMenuItem
+            // 
+            this.refreshHTMLToolStripMenuItem.Name = "refreshHTMLToolStripMenuItem";
+            this.refreshHTMLToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.refreshHTMLToolStripMenuItem.Text = "Refresh HTML";
+            this.refreshHTMLToolStripMenuItem.Click += new System.EventHandler(this.refreshHTMLToolStripMenuItem_Click);
+            // 
+            // developedByClxyifyToolStripMenuItem
+            // 
+            this.developedByClxyifyToolStripMenuItem.Name = "developedByClxyifyToolStripMenuItem";
+            this.developedByClxyifyToolStripMenuItem.Size = new System.Drawing.Size(130, 20);
+            this.developedByClxyifyToolStripMenuItem.Text = "Developed By Clxyify";
+            this.developedByClxyifyToolStripMenuItem.Click += new System.EventHandler(this.developedByClxyifyToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(324, 20);
+            this.toolStripMenuItem1.Text = "-------------------------------------------------------------";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 493);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(517, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Site here, make sure it ends with .html otherwise it won\'t download properly!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(524, 497);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Paste a URL to download the HTML!";
+            // 
+            // Compiler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 518);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.fastColoredTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Compiler";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hypertext Markup Language Compiler - Compiles your HTML with 100% Accuracy!";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveHTMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshHTMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem developedByClxyifyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HTML Compiler/HTML Compiler/Compiler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTML_Compiler
+{
+    public partial class Compiler : Form
+    {
+        public Compiler()
+        {
+            InitializeComponent();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string html = fastColoredTextBox1.Text;//grab HTML
+            if (textBox1.Text.EndsWith(".html")) 
+            {
+                MessageBox.Show("URL box contains a URL, downloading.\n\nBy the way make sure it contains .html at the end!","Mr. Mike Cox");
+                WebClient ccc = new WebClient();
+                //made by clxyify
+                webBrowser1.DocumentText = ccc.DownloadString(textBox1.Text);
+                fastColoredTextBox1.Text = ccc.DownloadString(textBox1.Text);
+                textBox1.Text = "";
+
+            }
+            else 
+            {
+                try
+                {
+                    webBrowser1.DocumentText = html + "<p>Developed by Clxyify! - github.com/clxyify </p>";//yep it's this simple!
+                }
+                catch (Exception)
+                {
+                    ;//my enemy
+
+                    webBrowser1.DocumentText = "<p>HTML could not be compiled successfully.</p>";//return an error
+                }
+            }
+        }
+
+        private void refreshHTMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();//refresh browserrrrrr
+        }
+
+        private void developedByClxyifyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void saveHTMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.IO.File.WriteAllText("HTML_saved.html", fastColoredTextBox1.Text + "<p>Developed by Clxyify - Saved HTML btw.</p>");
+        }
+    }
+}
